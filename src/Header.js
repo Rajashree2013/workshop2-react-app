8bc62a4d33e5d9944a4007389fd62c7dbc24fad9
Rajashree <bhuyan.rajashree@gmail.com>
2022-09-22 21:27:23 +0200

    WorkShop 2 Project setup

---
+++ src/Header.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Person from './components/Person';
+import Welcome from './components/Welcome';
+
+class App extends Component {
+  render() {
+    return (
+    <Router>
+        <div>
+          <h2>Welcome to React Router Tutorial</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to={'/'} className="nav-link"> Home </Link></li>
+            <li><Link to={'/welcome'} className="nav-link"> Welcome </Link></li>
+            <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
+            <li><Link to={'/about'} className="nav-link">About</Link></li>
+            <li><Link to={'/person'} className="nav-link">Person</Link></li>
+          </ul>
+          </nav>
+          <hr />
+          <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/welcome' component={Welcome} />
+              <Route path='/contact' component={Contact} />
+              <Route path='/about' component={About} />
+              <Route path='/person' component={Person} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Person.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Person extends Component {
+  render() {
+    return (
+        <div>
+          <h2>Person</h2>
+        </div>
+    );
+  }
+}
+
+export default Person;
